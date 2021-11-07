193e193f71452f8b86a6de2d44389110a47b8f21
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-07 09:52:20 +0530

    door lock mechanism partly done

---
+++ Assets/Scripts/insertDiamond.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class insertDiamond : MonoBehaviour, IInteractable
+{
+
+    public string UnlockItem;
+    public string lock_name;
+
+    private GameObject inventory;
+
+    private GameObject diamond_lock;
+
+    public void Interact(DisplayImage currentDisplay)
+    {
+        // if the current selected slot has an item whose name is the same as UnlockItem, then
+        if (inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem)
+        {
+            Debug.Log("unlock");
+            diamond_lock.SetActive(true);
+            inventory.GetComponent<Inventory>().currentSelectedSlot.GetComponent<Slot>().ItemProperty = Slot.property.empty;
+            inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite =
+                Resources.Load<Sprite>("Items/empty");
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.Find("Inventory");
+        diamond_lock = GameObject.Find(lock_name);
+        diamond_lock.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
